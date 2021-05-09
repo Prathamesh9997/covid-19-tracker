a40ab2baecbfd1c7648f6125182edb500db2322d
Prathamesh Gunde <gundep09@gmail.com>
2021-05-09 12:31:37 +0530

    header-part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
+//https://disease.sh/v3/covid-19/countries
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
 
-function App() {
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
   return (
     <div className="app">
-      <h1>Covid 19 tracker</h1>
+      <div className="app__header">
+        <h1>COVID 19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country, index) => (
+              <MenuItem key={index} value={country.value}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
